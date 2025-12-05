884e0fd01330706edd703f0cd5ae73ed2faa8cd1
raghavG0212 <raghavgoe0212@gmail.com>
2025-12-05 20:21:54 +0530

    fix

--- Vistaro-frontend-main/src/components/EventCard.jsx
+++ Vistaro-frontend-main/src/components/EventCard.jsx
@@ -3,6 +3,7 @@
 import React from "react";
 import { Box, Image, Text, Button } from "@chakra-ui/react";
 import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const fmtDateOnly = (iso) => {
   if (!iso) return "TBD";
